96c7b06856cfc5c824c315b3ac3b15ceaa7e38f5
fabimdev <108241268+fabimdev@users.noreply.github.com>
2023-12-01 17:28:31 -0300

    Up arc

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const correctAnswer = ["C", "B", "C", "D", "A", "D", "B", "B", "C", "C"];
+const correctAnswer = ["D", "B", "C", "B", "D"];
 const form = document.querySelector(".quiz-form");
 const result = document.querySelector(".result");
 const questions = document.querySelectorAll(".question");
@@ -7,18 +7,7 @@ form.addEventListener("submit", event => {
   event.preventDefault();
 
   let score = 0;
-  const userAnswers = [
-    form.q1.value,
-    form.q2.value,
-    form.q3.value,
-    form.q4.value,
-    form.q5.value,
-    form.q6.value,
-    form.q7.value,
-    form.q8.value,
-    form.q9.value,
-    form.q10.value,
-  ];
+  const userAnswers = [form.q1.value, form.q2.value, form.q3.value, form.q4.value, form.q5.value];
 
   userAnswers.forEach((answer, index) => {
     if (answer === correctAnswer[index]) {
@@ -31,5 +20,5 @@ form.addEventListener("submit", event => {
 
   scrollTo(0, 0);
   result.classList.remove("hide");
-  result.querySelector("p").textContent = `Você acertou ${score}/10`;
+  result.querySelector("p").textContent = `Sua pontuação foi ${score}/5!`;
 });
